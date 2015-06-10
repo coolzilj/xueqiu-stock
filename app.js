4843fcae3313e1468df785f80b46bb5967b9bb8a
Liu Jin <coolziljin@gmail.com>
2015-06-10 12:50:23 +0800

    use vuejs
    - fix color of 0% change
    - sort by key

--- app.js
+++ app.js
@@ -1,22 +1,60 @@
 var _   = require('lodash');
 var ipc = require('ipc');
 var fs  = require('fs');
+var Vue = require('vue');
 
-var templates = {
-  stocks: fs.readFileSync(__dirname + '/stocks.tmpl').toString()
-};
+Vue.component('stock-grid', {
+  template: '#stock-grid-template',
+  replace: true,
+  data: function () {
+    return {
+      columns: null,
+      sortKey: '',
+      reversed: {}
+    };
+  },
+  compiled: function () {
+    var self = this;
+    this.columns.forEach(function (column) {
+      self.reversed.$add(column.key, false);
+    });
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key;
+      this.reversed[key] = !this.reversed[key];
+    }
+  }
+});
+
+var stock = new Vue({
+  el: '#container',
+  data: {
+    gridColumns: [
+    {
+      key: 'code',
+      displayName: '股票'
+    },
+    {
+      key: 'current',
+      displayName: '当前价'
+    },
+    {
+      key: 'percentage',
+      displayName: '涨跌幅'
+    }],
+    gridData: []
+  },
+  methods: {
+    onOpenDir: function () {
+      ipc.send('open-dir');
+    },
+    onTerminate: function () {
+      ipc.send('terminate');
+    }
+  }
+});
 
 ipc.on('got-all', function gotAll(data) {
-  var content = _.template(templates.stocks)({
-    stocks: data
-  });
-  document.getElementById('container').innerHTML = content;
-});
-
-document.getElementById('open-dir').addEventListener('click', function () {
-  ipc.send('open-dir');
-});
-
-document.getElementById('quit').addEventListener('click', function () {
-  ipc.send('terminate');
+  stock.gridData = data;
 });
